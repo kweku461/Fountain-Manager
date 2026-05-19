f7352ac759f4fc18bb3545c9a61c6b6b22f66bf4
kweku461 <nanakwekuhwedie461@gmail.com>
2026-05-19 13:39:08 +0000

    Revert vite config to simple version

--- church-frontend/vite.config.ts
+++ church-frontend/vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
-  publicDir: 'public',
 })
